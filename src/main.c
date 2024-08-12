d2d4bb72af95aca47e32b2470b2161f5e0a21305
Blair Fitzhugh <bfitz1@umbc.edu>
2024-08-12 16:33:10 -0400

    Render a window filled with red

--- src/main.c
+++ src/main.c
@@ -29,15 +29,56 @@ bool initialize_window(void) {
     // Create an SDL renderer
     renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
-        fprintf("Error creating SDL renderer.\n");
+        fprintf(stderr, "Error creating SDL renderer.\n");
         return false;
     }
     
     return true;
 }
 
+void setup(void) {
+    // TODO: 
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+    case SDL_QUIT:
+        is_running = false;
+        break;
+    case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE) {
+            is_running = false;
+        }
+        break;
+    }
+}
+
+void update(void) {
+    // TODO: 
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // ...
+
+    SDL_RenderPresent(renderer);
+}
+
 int main(void) {
     is_running = initialize_window();
 
+    setup();
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+    
     return 0;
 }
