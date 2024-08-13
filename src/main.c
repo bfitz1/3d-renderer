32ffb6e8d84ac361101df3f51ae4cfbda7161468
Blair Fitzhugh <bfitz1@umbc.edu>
2024-08-13 13:00:50 -0400

    Add an array of points to make a cube

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 #include "display.h"
 #include "vector.h"
 
+// Declare an array of vectors/points
+const int N_POINTS = 9*9*9;
+vec3_t cube_points[N_POINTS]; // 9x9x9 cube
+
 bool is_running = false;
 
 void setup(void) {
@@ -20,6 +24,19 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    int point_count = 0;
+
+    // Start loading my array of vectors
+    // From -1 to 1 (in this 9x9x9 cube)
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
 }
 
 void process_input(void) {
@@ -59,8 +76,6 @@ int main(void) {
     is_running = initialize_window();
 
     setup();
-
-    vec3_t myvector = { 2.0, 3.0, -4.0 };
 
     while (is_running) {
         process_input();
