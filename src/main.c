54ac7b38721f6ac71e289f0ed9ca63dbe025ef7f
Blair Fitzhugh <bfitz1@umbc.edu>
2024-08-13 12:48:50 -0400

    Add vector structs

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h> // Search the SDL2 directory (system path) for SDL.h
 
 #include "display.h"
+#include "vector.h"
 
 bool is_running = false;
 
@@ -58,6 +59,8 @@ int main(void) {
     is_running = initialize_window();
 
     setup();
+
+    vec3_t myvector = { 2.0, 3.0, -4.0 };
 
     while (is_running) {
         process_input();
