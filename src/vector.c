aa3d6b6a72e4111b6038d192b1180d507a119dbb
Blair Fitzhugh <bfitz1@umbc.edu>
2024-08-16 10:47:14 -0400

    Add functions for vector addition, subtraction, scalar multiplication and scalar division

--- src/vector.c
+++ src/vector.c
@@ -2,6 +2,76 @@
 
 #include "vector.h"
 
+// 2D vector functions
+float vec2_length(vec2_t v) {
+    return sqrt(v.x * v.x + v.y + v.y);
+}
+
+vec2_t vec2_add(vec2_t a, vec2_t b) {
+    return (vec2_t) {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b) {
+    return (vec2_t) {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+}
+
+vec2_t vec2_mul(vec2_t v, float factor) {
+    return (vec2_t) {
+        .x = v.x * factor,
+        .y = v.y * factor,
+    };
+}
+
+vec2_t vec2_div(vec2_t v, float factor) {
+    return (vec2_t) {
+        .x = v.x / factor,
+        .y = v.y / factor,
+    };
+}
+
+// 3D vector functions
+float vec3_length(vec3_t v) {
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b) {
+    return (vec3_t) {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z
+    };
+}
+
+vec3_t vec3_sub(vec3_t a, vec3_t b) {
+    return (vec3_t) {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z
+    };
+}
+
+vec3_t vec3_mul(vec3_t v, float factor) {
+    return (vec3_t) {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor,
+    };
+}
+
+vec3_t vec3_div(vec3_t v, float factor) {
+    return (vec3_t) {
+        .x = v.x / factor,
+        .y = v.y / factor,
+        .z = v.z / factor,
+    };
+}
+
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
     vec3_t rotated_vector = {
         .x = v.x,
